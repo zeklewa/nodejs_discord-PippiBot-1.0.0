f4dbfb3b6a1e4098c374a023f647abd2dc88e658
Hoàng Bảo Phúc <phucdtd@gmail.com>
2018-05-08 14:45:09 +0700

    Revert "Phucdtd patch1.0"

--- custom_modules/uptime.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	uptime: function()
-	{
-		var uptime = + new Date() - start_up_time;
-     	console.log(uptime);
-        toChannel.send(log_mess + "I have been up for " + rqfunc.timeConverter(uptime) + ". Beep boop.*");
-        return;
-	}
-}
